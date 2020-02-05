6864c2afb3cf19a68ed5b8dc62bba191d402b7ce
Kristoffe Biglete <kristoffe.biglete@gmail.com>
2020-02-05 13:40:59 +0100

    Archive title + future talks

--- wp-content/plugins/heartland-content/Frontend/Type/Blog.php
+++ wp-content/plugins/heartland-content/Frontend/Type/Blog.php
@@ -33,7 +33,7 @@ class Blog implements Page {
 		}
 
 		if ( is_archive() ) {
-			$title      = get_the_archive_title();
+			$title      = $this->get_archive_title( get_the_archive_title() );
 			$shortcode  = do_shortcode( '[latest-posts number="' . get_option( 'posts_per_page' ) . '" category="' . get_queried_object()->slug . '"]' );
 			$categories = [];
 
@@ -72,5 +72,21 @@ class Blog implements Page {
 		$pagination = sprintf( '<div class="pagination">%s</div>', $list );
 
 		return $pagination;
+	}
+
+	function get_archive_title( $title ) {
+	    if ( is_category() ) {
+	        $title = single_cat_title( '', false );
+	    } elseif ( is_tag() ) {
+	        $title = single_tag_title( '', false );
+	    } elseif ( is_author() ) {
+	        $title = '<span class="vcard">' . get_the_author() . '</span>';
+	    } elseif ( is_post_type_archive() ) {
+	        $title = post_type_archive_title( '', false );
+	    } elseif ( is_tax() ) {
+	        $title = single_term_title( '', false );
+	    }
+
+	    return $title;
 	}
 }
